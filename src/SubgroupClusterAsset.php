6af84dc0d98d6a81b2a00d003e9a1b8d8d6dcb0a
Dmitriy Logachov <demonwork@yandex.ru>
2020-05-29 21:45:43 +0500

    Исправление зависимостей, отказ от старой версии MarkerClusterGroup.

--- src/SubgroupClusterAsset.php
+++ src/SubgroupClusterAsset.php
@@ -4,34 +4,10 @@ namespace koputo\leaflet\plugins\subgroup;
 
 use yii\web\AssetBundle;
 
-/**
- * LeafLetAsset Registers widget requires files. Please, use the following in order to override bundles for CDN:
- *
- * ```
- *  return [
- *        // ...
- *        'components' => [
- *            'bundles' => [
- *                'dosamigos\leaftlet\LeafLetAsset' => [
- *                    'sourcePath' => null,
- *                    'js' => [ 'http://cdn.leafletjs.com/leaflet-0.7.3/leaflet.js' ],
- *                    'css' => [ 'http://cdn.leafletjs.com/leaflet-0.7.3/leaflet.css' ]
- *                ]
- *            ]
- *        ]
- *    ]
- * ```
- *
- * @author Antonio Ramirez <amigo.cobos@gmail.com>
- * @link http://www.ramirezcobos.com/
- * @link http://www.2amigos.us/
- * @package dosamigos\leaflet
- */
 class SubgroupClusterAsset extends AssetBundle
 {
     public $depends = [
         'dosamigos\leaflet\LeafLetAsset',
-        'dosamigos\leaflet\plugins\markercluster\MarkerClusterAsset',
         ];
 
     public $css = [
@@ -46,7 +22,7 @@ class SubgroupClusterAsset extends AssetBundle
         $this->sourcePath = __DIR__ . '/assets';
         $this->js = [
             'js/subgroup.js',
-            'js/leaflet.markercluster.js',
+            'js/markercluster.js',
         ];
         $this->css = ['css/MarkerCluster.css'];
     }
